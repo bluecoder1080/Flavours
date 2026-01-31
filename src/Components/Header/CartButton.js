9431ebdddbeb97e1724de89e69cd0f5eee17321b
bluecoder1080 <adityasingh1080z@gmail.com>
2026-01-31 12:44:58 +0530

    feat: Implement CartButton component with item count display and cart toggle functionality.

--- src/Components/Header/CartButton.js
+++ src/Components/Header/CartButton.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Cart from './Components/Cart/Cart';
-import { useCart } from './context/CartContext';
+import Cart from '../Cart/Cart';
+import { useCart } from '../../context/CartContext';
 
 const CartButton = () => {
   const { itemCount } = useCart();
